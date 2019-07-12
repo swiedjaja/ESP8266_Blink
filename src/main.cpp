fbc542caf729bb4f3d7b2ce6d861cd0b5b2f009d
Steffanus W <swiedjaja@gmail.com>
2019-07-12 22:44:41 +0700

    Ver-1.0 Initial release

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
+  delay(3000);
 }
